4c133c16c9fcc98c7c6038aba96086d467de2624
hana <160441131@ccalumni.meijo-u.ac.jp>
2020-10-21 18:56:16 +0900

    画像投稿部分を使用しやすく変更． アップロードした写真から位置情報を取得するためにjquery.exif.jsを追加

--- js/post.js
+++ js/post.js
@@ -4,4 +4,19 @@ function previewImage (obj) {
 		document.getElementById('preview').src = fileReader.result;
 	});
 	fileReader.readAsDataURL(obj.files[0]);
+
+	$("#preview").fileExif(function(exif) {
+		if (!exif) {
+			console.log("exif情報なし");
+			return;
+		}
+		if (!exif.GPSLatitude) {
+			console.log("GPS情報なし");
+			return;
+		}
+		var lat = exif.GPSLatitude[0]  + (exif.GPSLatitude[1] / 60)  + (exif.GPSLatitude[2] / 3600);
+		var lng = exif.GPSLongitude[0] + (exif.GPSLongitude[1] / 60) + (exif.GPSLongitude[2] / 3600);
+		console.log({lat,lng}); // google maps とかで使える形式
+	});
+	
 }
