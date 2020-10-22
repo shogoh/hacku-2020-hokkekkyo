c757839b5fdf02f3781a8f93a36420ac0b93dfed
guchi0 <sp9g83t7@gmail.com>
2020-10-22 20:38:41 +0900

    岩口再

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 
+/*マップの定義 leafletを使用 */
 let mymap=L.map("map")
-mymap.setView([35.1356448, 136.9760683], 16);//初期位置、ズームレベル
+mymap.setView([35.1356448, 136.9760683], 17);//初期位置、ズームレベル
 
 new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
   {
@@ -8,42 +9,46 @@ new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
   maxZoom: 18
 }).addTo(mymap)
 
+//オープンストリートマップを使用（デフォルトは地理院地図）
+// let osm = L.tileLayer('http://tile.openstreetmap.jp/{z}/{x}/{y}.png',
+//     {attribution: "<a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors"});
+// osm.addTo(mymap);
 
-let marker_meijo = L.marker([35.1356448, 136.9760683]).addTo(mymap);
-marker_meijo.bindPopup("<p>名城大学</p>")
-//marker.bindPopup("<p>地図を表示タイミングでポップアップ</p>").openPopup();
 
-// 現在の位置情報
 
-// Geolocation APIに対応しているか
-if( navigator.geolocation )
-{
-  // 現在位置を取得できる場合の処理
-  alert( "あなたの端末では、現在位置を取得することができます。" ) ;
-}
+let watch_id;// イベントハンドラid,
+let dist;// 目的地の位置情報, 
+let cnt = 0;//ヒントを与えた回数
 
-// Geolocation APIに対応していない
-else
-{
-  // 現在位置を取得できない場合の処理
-  alert( "あなたの端末では、現在位置を取得できません。" ) ;
-}
-function calc_euclid_dist(lat1, lon1, lat2, lon2, mode=true) {
+// 目的地の位置をマップに表示（仮）
+dist = L.marker([35.1356448, 136.9760683]).addTo(mymap).bindPopup("<p>名城大学</p>");
+
+let now_lat;　//現在の緯度
+let now_lon; //現在の経度
+let dist_lat = dist._latlng.lat; //目的地の緯度
+let dist_lon = dist._latlng.lng; //目的地の経度
+
+
+  /*2点間距離を算出するプログラム */
+function calc_euclid_dist(mode=true) {
+
   // 参考ページ；https://qiita.com/chiyoyo/items/b10bd3864f3ce5c56291(ヒュベニの公式)
+  // mode: true -> 世界測地系; false -> 日本測地系.
+
   // 緯度経度をラジアンに変換
-  let radLat1 = (lat1) * (Math.PI / 180); // 緯度１
-  let radLon1 = (lon1) * (Math.PI / 180); // 経度１
-  let radLat2 = (lat2) * (Math.PI / 180); // 緯度２
-  let radLon2 = (lon2) * (Math.PI / 180); // 経度２
+  let radnow_lat = (now_lat) * (Math.PI / 180); // 緯度１
+  let radnow_lon = (now_lon) * (Math.PI / 180); // 経度１
+  let raddist_lat = (dist_lat) * (Math.PI / 180); // 緯度２
+  let raddist_lon = (dist_lon) * (Math.PI / 180); // 経度２
 
   // 緯度差
-  let radLatDiff = radLat1 - radLat2;
+  let radLatDiff = radnow_lat - raddist_lat;
 
   // 経度差算
-  let radLonDiff = radLon1 - radLon2;
+  let radLonDiff = radnow_lon - raddist_lon;
 
   // 平均緯度
-  let radLatAve = (radLat1 + radLat2) / 2.0;
+  let radLatAve = (radnow_lat + raddist_lat) / 2.0;
 
   // 測地系による値の違い
   let a = mode ? 6378137.0 : 6377397.155; // 赤道半径
@@ -62,6 +67,9 @@ function calc_euclid_dist(lat1, lon1, lat2, lon2, mode=true) {
   let t2 = N * Math.cos(radLatAve) * radLonDiff;
   let dist = Math.sqrt((t1*t1) + (t2*t2));
 
+  // 小数点以下を切り捨てる
+  dist = dist.toFixed();
+
   if (dist >= 1000) {
     dist /= 1000;
     dist = dist + "km";
@@ -71,34 +79,115 @@ function calc_euclid_dist(lat1, lon1, lat2, lon2, mode=true) {
   }
   return dist;
   }
-function successFunc(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
 
-  alert("緯度:" + latitude + ", 経度:" + longitude + ",名城大までの距離:" + calc_euclid_dist(latitude, longitude,35.1356448, 136.9760683));
-  let marker_now = L.marker([latitude, longitude]).addTo(mymap);
-  marker_now.bindPopup("<p>現在地</p>");
-  mymap.setView([latitude, longitude], 16);//初期位置、ズームレベル
+// 現在地からみた目的地の方角を角度で算出
+function calc_direction () {
+
+  // 緯度経度をラジアンに変換
+  let radnow_lat = (now_lat) * (Math.PI / 180); // 緯度１
+  let radnow_lon = (now_lon) * (Math.PI / 180); // 経度１
+  let raddist_lat = (dist_lat) * (Math.PI / 180); // 緯度２
+  let raddist_lon = (dist_lon) * (Math.PI / 180); // 経度２
+
+  // 経度差
+  let radLonDiff = radnow_lon - raddist_lon;
+
+  let arg1 = Math.sin(radLonDiff);
+  let arg2 = Math.cos(radnow_lat) * Math.tan(raddist_lat) - Math.sin(radnow_lat) * Math.cos(radLonDiff);
+
+  let dir = 90.0 - Math.atan2(arg1, arg2);
+
+  // 小数点2桁以下を切り捨てる
+  dir = dir.toFixed(2);
+
+  // 16方位から適切なものを選択
+  int_dir = ["北北東","北東","東北東","東","東南東","南東","南南東","南","南南西","南西","西南西","西","西北西","北西","北北西","北"]
+  let left = 12.5; 
+  for (var i = 0;i < 15;i++) {
+    if (left <= dir && dir < (left + 22.5)){
+      dir = int_dir[i];
+      break;
+    }
+    if (i == 14){
+      dir = int_dir[15];
+    }
+    left += 22.5;
+  }
+
+  return dir;
+}
+
+// ヒントを与える
+function calc_hint(pos) {
+
+  if (cnt == 0){
+    //現在地から目的地までの方角を算出(1回目のヒント)
+    // window.alert("方角:" + calc_direction() + "\n方位角は北:0度、東:90度、南:180度、西:270度");
+    document.getElementById("hintText").textContent = "方角:" + calc_direction() + "\n";
+  }
+  else if (cnt == 1){
+    // 現在地から目的地までの距離を算出（2回目のヒント）
+    // window.alert("緯度:" + now_lat + ", 経度:" + now_lon + ",写真の場所までの距離:" + calc_euclid_dist());
+    document.getElementById("hintText").textContent += "距離:" + calc_euclid_dist();
+  }
+
 };
+
+	// エラーコードのメッセージを定義
 function errorFunc( error )
 {
-	// エラーコードのメッセージを定義
-	var errorMessage = {
+	let errorMessage = {
 		0: "原因不明のエラーが発生しました…。" ,
 		1: "位置情報の取得が許可されませんでした…。" ,
 		2: "電波状況などで位置情報が取得できませんでした…。" ,
 		3: "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。" ,
 };
-
 	// エラーコードに合わせたエラー内容をアラート表示
 	alert( errorMessage[error.code] ) ;
 }
+
 // オプション・オブジェクト
-var optionObj = {
-	"enableHighAccuracy": false ,
-	"timeout": 8000 ,
-	"maximumAge": 5000 ,
+/*https://developer.mozilla.org/ja/docs/Web/API/PositionOptions:*/
+let optionObj = {
+  // 高精度を必要とするか
+  "enableHighAccuracy": false ,
+  // 位置情報取得時間の上限
+  "timeout": 8000 ,
+  // 一度取得した位置情報の有効期限
+	"maximumAge": 3000 ,
 };
 
-navigator.geolocation.getCurrentPosition(successFunc , errorFunc, optionObj);
+// 現在地の更新を行う && ゴール判定
+function successFunc (pos) {
+  now_lat = pos.coords.latitude;　//現在の緯度の更新
+  now_lon = pos.coords.longitude; //現在の経度の更新
 
+  // リアルタイムの現在の自分の位置をマップに表示
+  L.marker([now_lat, now_lon]).addTo(mymap);
+  mymap.setView([now_lat, now_lon]);//現在場所、ズームレベル
+
+  // 精度を円で表現（ガバすぎるので未設定）
+  // var radius = pos.coords.accuracy;
+  // L.circle([now_lat, now_lon], radius).addTo(mymap);
+
+
+  // 位置情報が現在地==目的地であるならばゴールと判定
+  if (now_lat == dist_lat && now_lon == dist_lon){
+    alert('Congratulations!! \n 写真の場所を見つけたよ！');
+    navigator.geolocation.clearWatch(watch_id);
+  }
+}
+
+// ヒントとなる目的地までの距離,方角を算出
+function get_hint() {
+  if (confirm("投稿しますか？")) {
+    // ヒントを与える
+    calc_hint();
+
+    // ヒント回数をインクリメント
+    cnt++;
+  }
+}
+
+// 定期的にユーザの位置情報を取得　idには登録されたハンドラを識別するIDが入る
+watch_id = navigator.geolocation.watchPosition(successFunc, errorFunc, optionObj);
--- js/post.js
+++ js/post.js
@@ -4,4 +4,23 @@ function previewImage (obj) {
 		document.getElementById('preview').src = fileReader.result;
 	});
 	fileReader.readAsDataURL(obj.files[0]);
+
+	$("#upload-picture-button").fileExif(function(exif) {
+		document.getElementById("longitude").value = "hoge";
+		if (!exif) {
+			console.log("exif情報なし");
+			return;
+		}
+		if (!exif.GPSLatitude) {
+			console.log("GPS情報なし");
+			return;
+		}
+		var lat = exif.GPSLatitude[0]  + (exif.GPSLatitude[1] / 60)  + (exif.GPSLatitude[2] / 3600);
+		var lng = exif.GPSLongitude[0] + (exif.GPSLongitude[1] / 60) + (exif.GPSLongitude[2] / 3600);
+		// console.log({lat,lng}); // google maps とかで使える形式
+
+		document.getElementsById("longitude").value = lng;
+		document.getElementsById("latitude").value = lat;
+	});
+	
 }
